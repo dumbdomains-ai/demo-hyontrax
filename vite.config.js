b45fa41ea1a14da337d33fc510c20f269265d59b
Jain, Saiyam <saijain@teksystems.com>
2026-07-25 22:44:02 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
+  base: '/demo-hyontrax/',
   plugins: [react()],
   css: {
     postcss: {
